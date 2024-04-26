356f33f8b34b0afb13e66c6f8009a1a0102b1161
Sushant Singh <sushantbibhu@gmail.com>
2024-04-26 12:30:37 +0530

    "Add CORS middleware and root endpoint to server/app.js"

--- server/app.js
+++ server/app.js
@@ -1,5 +1,17 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("GET request to the homepage");
+});
 
 export default app;
